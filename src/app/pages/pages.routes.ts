f2cde9144c1463b27e7b9489398838d9f678ce6c
Jose Luis <jls0107@gmail.com>
2020-07-09 18:05:57 -0300

    detalles visuales

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -15,10 +15,10 @@ const pagesRoutes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: 'account-settings', component: AccountSettingsComponent },
+        { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+        { path: 'progress', component: ProgressComponent, data: { titulo: 'Dashboard' } },
+        { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Graficos' } },
+        { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes Visuales' } },
         { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
 }
