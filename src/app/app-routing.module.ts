5d1fe9313d5d38b20958a1d907882f6918cc5472
Jose Luis <jls0107@gmail.com>
2020-07-08 21:04:21 -0300

    primera ver

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+
+import { Routes, RouterModule} from '@angular/router';
+
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: NopagefoundComponent}
+];
+
+export const AppRoutingModule = RouterModule.forRoot( routes, { useHash: true } ) ;
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+// modulos
+import { Pagesmodule } from './pages/pages.module';
+
+// rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// componentes
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+// temportal
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    Pagesmodule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
